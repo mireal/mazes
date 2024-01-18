376beabac81b46e50d976c5c6e04c7c8c6cdefa1
mireal <moveloader@yandex.ru>
2024-01-18 23:42:08 +0300

    layout redesign
    Minor code fixed, changed layout

--- layout.py
+++ layout.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 from maze_generators import maze_filler
-from tools import add_tuple, get_direction, reverse_tuple, directions, remove_border, create_empty_maze
+from tools import get_direction, reverse_tuple, directions, remove_border, create_empty_maze
 
 
 class CanvasCell:
@@ -82,9 +82,14 @@ class DynamicCanvas:
         """Call erase method for graph object, clear object dictionary"""
         self.canvas.erase()
         self.canvas_objects.clear()
+        self.highlighted_cell = None
+
+    def change_title(self, new_title):
+        self.textbox.update(value=new_title)
 
     def draw_finished_maze(self, maze):
         self.clear()
+        # self.change_cell_color((0, 0), self.highlighted_cell_color)
         for y, row in enumerate(maze):
             for x, cell in enumerate(row):
                 pos = (y, x)
@@ -120,11 +125,11 @@ class DynamicCanvas:
 class DynamicLayout:
     def __init__(self, generators: list, solvers: list, cols, rows, cell_size,
                  current_state='Generate'):
-        self.window: sg.Window = None
+        self.window = None
 
         self.running = True
         self.paused = True
-        self.speed = 10
+        self.speed = 1000
         self.current_state = current_state
 
         self.cols, self.rows = cols, rows
@@ -135,27 +140,43 @@ class DynamicLayout:
 
         self.algorithm_objects = []
         self.mazes = []
-        self.update_algorithms(current_state)
-
-        self.basic_maze_generator = generators[2]
-        self.basic_maze = None
-
-        self.maze_generator_choice = sg.Column([[
-            sg.Text('Maze generator:'),
-            # sg.Combo(self.generator_names, default_value=self.basic_maze_generator, visible=False, enable_events=True),
-            sg.Push()]], visible=False)
-
         self.grid, self.canvas_objects = self.initialize_canvases()
 
-        self.control_panel = sg.Column([
-            [sg.Push(), sg.Button('Generate'), sg.Button('Solve'), sg.Push()],
-            [self.maze_generator_choice],
-            [sg.Push(), sg.Button('Start'), sg.Button('Pause'), sg.Push()],
-            [sg.Button('Slower'), sg.Push(), sg.Button('Faster'), sg.Push()],
-            [sg.Push(), sg.Button('Refresh'), sg.Push()],
-        ])
+        self.generator_names_map = {generator.__name__: generator for generator in generators}
+        generator_names = [generator.__name__ for generator in generators]
 
-        self.base_layout = [self.grid, self.control_panel]
+        self.basic_maze_generator = self.generator_names_map[generator_names[0]]
+        self.basic_maze = None
+        button_size = (15, 2)
+
+        self.choice_box = sg.Combo(generator_names, default_value=generator_names[0], enable_events=True,
+                                   key='-GENERATOR_CHOICE-', disabled=True)
+
+        start_pause_button = sg.Button('Start / Pause', key='-STARTPAUSE-', size=button_size)
+        refresh_button = sg.Button('↻', key='-REFRESH-', size=(2, 2))
+
+        speed_plus_button = sg.Button('+', key='-FASTER-', size=(2, 2))
+        speed_minus_button = sg.Button('-', key='-SLOWER-', size=(2, 2))
+        speed_text = sg.Button('Speed', size=(10, 2), mouseover_colors=('black', 'white'))
+
+        self.control_panel = sg.Column([
+            [sg.Text('')],
+            [sg.Button('Generate', size=button_size), sg.Push(), sg.Button('Solve', size=button_size)],
+            [sg.Text('')],
+            [sg.Push(), start_pause_button, refresh_button, sg.Push()],
+            [sg.Text('')],
+            [sg.Push(), speed_plus_button, speed_text, speed_minus_button, sg.Push()],
+            [sg.Text('')],
+            [sg.HSeparator()],
+            [sg.Text('')],
+            [sg.Text('Maze for solving: '), self.choice_box]
+        ], expand_y=True)
+
+        self.layout = [self.grid, sg.VSeparator(), self.control_panel]
+
+    def initialize(self, window):
+        self.window = window
+        self.refresh()
 
     def initialize_canvases(self):
         canvases = []
@@ -171,48 +192,29 @@ class DynamicLayout:
 
         return grid, canvas_objects
 
-    def update_canvases(self):
-        """Deletes all previously drawn rectangles and lines from canvases.
-        If the current state is Solve - ads finished maze to all canvases. """
-        self.paused = True
-        if self.current_state == 'Solve':
+    def refresh(self):
+        if self.current_state == 'Generate':
+            self.basic_maze = None
+            class_instances = self.generators
+            argument = (self.cols, self.rows)
+            self.mazes = [create_empty_maze(self.cols, self.rows) for _ in range(4)]
+        else:
+            class_instances = self.solvers
             self.basic_maze = create_empty_maze(self.cols, self.rows)
             maze_filler(self.basic_maze, self.basic_maze_generator((self.cols, self.rows)))
-        else:
-            self.basic_maze = None
+            argument = self.basic_maze
 
-        for canvas in self.canvas_objects:
+        class_objects = [inst(argument) for inst in class_instances]
+
+        for class_object, canvas in zip(class_objects, self.canvas_objects):
+            class_name = class_object.__class__.__name__
             canvas.clear()
+            canvas.change_title(class_name)
             if self.basic_maze:
                 canvas.draw_finished_maze(self.basic_maze)
                 canvas.change_cell_color((0, 0), '#58846d')
 
-    def update_algorithms(self, state):
-        """Updates list of algorithms based on given state"""
-        algorithms = []
-        algorithm_objects = []
-        args = ()
-        if state == 'Generate':
-            algorithms = self.generators
-            args = (self.cols, self.rows)
-            self.mazes = [create_empty_maze(self.cols, self.rows) for i in range(4)]
-        else:
-            algorithms = self.solvers
-            args = self.basic_maze
-
-        for i in range(4):
-            algorithm = algorithms[i](args)
-            algorithm_objects.append(algorithm)
-
-        self.algorithm_objects = algorithm_objects
-
-    def update_current_state(self, state):
-        self.current_state = state
-        self.update_canvases()
-
-        visibility = True if state == 'Solve' else False
-        self.maze_generator_choice.update(visible=visibility)
-        self.update_algorithms(state)
+        self.algorithm_objects = class_objects
 
     def generator_move(self, algorithm, canvas, maze):
         # maze_filler(maze, algorithm, step_by_step=True)
@@ -242,12 +244,11 @@ class DynamicLayout:
     def solver_move(self, algorithm, canvas):
         algorithm.move()
         curr_pos = algorithm.curr
-        canvas.change_cell_color(curr_pos, '#58846d')
+        canvas.change_cell_color(curr_pos, canvas.highlighted_cell_color)
 
     def move(self):
-        if all(alg.not_finished == False for alg in self.algorithm_objects):
+        if all(not alg.not_finished for alg in self.algorithm_objects):
             self.paused = True
-            print('Done')
             return
         for algorithm, canvas, maze in zip(self.algorithm_objects, self.canvas_objects, self.mazes):
             if algorithm.not_finished:
@@ -262,58 +263,50 @@ class DynamicLayout:
             self.running = False
             return
         elif event in ('Generate', 'Solve') and event != self.current_state:
-            self.update_current_state(event)
-        elif event == 'Refresh':
-            self.update_current_state(self.current_state)
-        elif event in ('Start', 'Pause'):
-            if event == 'Start' and self.paused:
-                self.paused = False
-            elif event == 'Pause' and not self.paused:
-                self.paused = True
-
-        elif event in ('Faster', 'Slower'):
-            if event == 'Slower':
-                self.speed += 100
-            elif self.speed > 50:
-                self.speed -= 50
+            # self.update_current_state(event)
+            self.current_state = event
+            self.refresh()
+            disable = False if event == 'Solve' else True
+            self.choice_box.update(disabled=disable)
+        elif event == '-REFRESH-':
+            self.paused = True
+            self.refresh()
+        elif event == '-STARTPAUSE-':
+            self.paused = not self.paused
+        elif event == '-FASTER-':
+            self.speed //= 2
             print(self.speed)
-
+        elif event == '-SLOWER-':
+            if self.speed < 2:
+                self.speed += 1
+            else:
+                self.speed *= 2
+            print(self.speed)
+        elif event == '-GENERATOR_CHOICE-':
+            name = values[event]
+            self.basic_maze_generator = self.generator_names_map[name]
+            self.refresh()
         if not self.paused:
             self.move()
 
-    def map_names_to_classes(self, classes: list):
-        classes_map = {}
-        for class_instance in classes:
-            name = self.get_class_name(class_instance)
-            classes_map[name] = class_instance
-
-        return classes_map
-
-    def get_class_name(self, class_instance):
-        return class_instance.__class__.__name__
-
 
 if __name__ == '__main__':
-    from maze_generators import RandomizedDFS, RandomizedPrim, HuntAndKill, maze_filler, PlaceholderGenerator
-    from tools import create_empty_maze
+    from maze_generators import RandomizedDFS, RandomizedPrim, HuntAndKill, PlaceholderGenerator
     from maze_solvers import DFS, BFS, PriorityDFS, PlaceholderSolver
 
-    generators = [RandomizedDFS, RandomizedPrim, HuntAndKill, PlaceholderGenerator]
-    solvers = [DFS, BFS, PriorityDFS, PlaceholderSolver]
+    generator_classes = [RandomizedDFS, RandomizedPrim, HuntAndKill, PlaceholderGenerator]
+    solver_classes = [DFS, BFS, PriorityDFS, PlaceholderSolver]
 
-    cols = rows = 80
-    cell_size = 5
+    col_len = row_len = 40
+    size = 10
 
-    sg.theme('Dark')
+    sg.theme('DarkTeal6')
 
-    LayoutController = DynamicLayout(generators, solvers, cols, rows, cell_size)
+    LayoutController = DynamicLayout(generator_classes, solver_classes, col_len, row_len, size)
 
-    separator = sg.VSeparator()
+    window = sg.Window('Window Title', [LayoutController.layout], finalize=True, return_keyboard_events=True)
 
-    layout = [LayoutController.grid, separator, LayoutController.control_panel]
-    window = sg.Window('Window Title', [layout], finalize=True, return_keyboard_events=True)
-
-    LayoutController.window = window
+    LayoutController.initialize(window)
 
     while LayoutController.running:
         LayoutController.event_handler()
